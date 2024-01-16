57db6302df618c122ad16712054bcf515b47c5ea
briansone <brian.hooke@mason.build>
2024-01-16 19:16:40 +1100

    should have postgres working

--- builder_app/settings.py
+++ builder_app/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'builder_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://mydatabaseuser:mypassword@localhost/builder_app_db', conn_max_age=600)
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
